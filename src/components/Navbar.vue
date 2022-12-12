09a663adaaf58deee9b99f1bceb131a718bb66eb
Maks1842 <maks1842@yandex.ru>
2022-12-12 22:20:00 +0300

    Start Project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-app-bar
+          color="deep-purple accent-4"
+          dark
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Page title</v-toolbar-title>
+
+        <v-spacer/>
+        <v-btn outlined class="ml-3">Главная</v-btn>
+        <v-btn outlined class="ml-3">Фото</v-btn>
+
+      </v-app-bar>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/PhotosPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    PHOTO
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PhotosPage"
+}
+</script>
+
+<style scoped>
+
+</style>
